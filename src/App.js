bd1c5f74b428ee0fb17c7ecac9b57da513d4f907
Rafał Łukawski <rs.lukawski@gmail.com>
2021-08-25 10:29:15 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./App.scss";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
