2cd99f30c2f4026be0975f60557db7edde79647e
Rafał Łukawski <rs.lukawski@gmail.com>
2021-08-27 12:16:41 +0200

    miltiple streams

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Streams from "./components/Streams";
-
-// import "./App.scss";
 
 import Icon from "./components/Icon";
 import Brand from "./components/Brand";
@@ -375,137 +373,7 @@ function App() {
         <div className="content-main">
           <Route path="/watch" component={Stream} />
         </div>
-        <div className="content-right">
-          {videoItems}
-          <div className="video-item">
-            <img src={stream1Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">C-BooL - Golden Rules</h3>
-              <span className="desc-author">C-BooL</span>
-              <br />
-              <span className="desc-views">9.7M views</span>
-              <span className="desc-time">4 months ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream2Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Tina Turner - Mother Within (Heavenly Home) - 'Beyond'
-              </h3>
-              <span className="desc-author">TINA Turner Blog</span>
-              <br />
-              <span className="desc-views">2.4M views</span>
-              <span className="desc-time">6 years ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream3Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Sia - Courage To Change (Official Music Video)
-              </h3>
-              <span className="desc-author">Sergey Tyapaev</span>
-              <br />
-              <span className="desc-views">14M views</span>
-              <span className="desc-time">6 months ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream4Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Rag'n'Bone Man - Human (Official Video)
-              </h3>
-              <span className="desc-author">Rag'n'Bone Man</span>
-              <br />
-              <span className="desc-views">1.3B views</span>
-              <span className="desc-time">5 years ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream5Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Sadhguru On How to Manifest What you Really Want
-              </h3>
-              <span className="desc-author">Sadhguru</span>
-              <br />
-              <span className="desc-views">12M views</span>
-              <span className="desc-time">1 year ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream6Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">Pharell Williams - Freedom (Video)</h3>
-              <span className="desc-author">Pharell Williams</span>
-              <br />
-              <span className="desc-views">114M views</span>
-              <span className="desc-time">6 years ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream7Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Will Smith - Black Suits Comin' (Nod Ya Head) ft. TRÂ-Knox
-              </h3>
-              <span className="desc-author">WillSmithVEVO</span>
-              <br />
-              <span className="desc-views">12M views</span>
-              <span className="desc-time">11 years ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream8Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Imagine Dragons - Whatever It Takes
-              </h3>
-              <span className="desc-author">ImagineDragons</span>
-              <br />
-              <span className="desc-views">70M views</span>
-              <span className="desc-time">4 years ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream9Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Coldplay - Adventure Of A Lifetime (Official Video)
-              </h3>
-              <span className="desc-author">Coldplay</span>
-              <br />
-              <span className="desc-views">1.2B views</span>
-              <span className="desc-time">6 years ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream10Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Katy Perry - Dark Horse (Official) ft. Juicy J
-              </h3>
-              <span className="desc-author">Katy Perry</span>
-              <br />
-              <span className="desc-views">3.1B views</span>
-              <span className="desc-time">7 years ago</span>
-            </div>
-          </div>
-          <div className="video-item">
-            <img src={stream11Img} className="item-img" alt="item"></img>
-            <div className="item-desc">
-              <h3 className="desc-title">
-                Ed Sheeran - Shape of You (Official Music Video)
-              </h3>
-              <span className="desc-author">Ed Sheeran</span>
-              <br />
-              <span className="desc-views">5.4B views</span>
-              <span className="desc-time">4 years ago</span>
-            </div>
-          </div>
-        </div>
+        <div className="content-right">{videoItems}</div>
       </div>
     </Router>
   );
--- src/components/Streams.js
+++ src/components/Streams.js
@@ -13,6 +13,14 @@ import stream11Img from "../streams/ed-sheeran-shape-of-you/stream.jpg";
 import stream1Vid from "../streams/c-bool-golden-rules/stream.webm";
 import stream2Vid from "../streams/tina-mother-within/stream.webm";
 import stream3Vid from "../streams/sia-courage-to-change/stream.webm";
+import stream4Vid from "../streams/rag-n-bone-human/stream.webm";
+import stream5Vid from "../streams/sadhguru-manifest/stream.mp4";
+import stream6Vid from "../streams/pharell-williams-freedom/stream.webm";
+import stream7Vid from "../streams/will-smith-black-suits/stream.webm";
+import stream8Vid from "../streams/imagine-dragons-whatever/stream.webm";
+import stream9Vid from "../streams/coldplay-adventure-of-a-lifetime/stream.webm";
+import stream10Vid from "../streams/katy-perry-dark-horse/stream.webm";
+import stream11Vid from "../streams/ed-sheeran-shape-of-you/stream.mp4";
 
 const Streams = [
   {
@@ -59,13 +67,150 @@ const Streams = [
     viewsSmall: "48M",
     releaseDate: "Jan 2, 2021",
     releaseAgo: "8 months ahgo",
-    author: " Sergey Tyapaev",
+    author: "Sergey Tyapaev",
     subscribers: "13.2K",
     likes: "131K",
     unlikes: "2.3K",
     description: `#sia #couragetochange #queensgambit #anyataylorjoy #chess #vevo`,
     image: stream3Img,
     video: stream3Vid,
+  },
+  {
+    id: "L3wKzyIN1yk",
+    tags: ["RagnBoneMan", "Human", "RagnBoneManOfficial"],
+    title: "Rag'n'Bone Man - Human (Official Video)",
+    views: "1,344,859,914",
+    viewsSmall: "1.34B",
+    releaseDate: "Jul 21, 2016",
+    releaseAgo: "5 years ago",
+    author: "Rag'n'Bone Man",
+    subscribers: "3.38M",
+    likes: "8.9M",
+    unlikes: "265K",
+    description: `Rag'n'Bone Man - Human (Official Video)
+    Stream Rag'n'Bone Man here: https://ragnboneman.lnk.to/Playlists
+    Subscribe to Rag'n'Bone Man's YouTube Channel: https://ragnboneman.lnk.to/YouTube`,
+    image: stream4Img,
+    video: stream4Vid,
+  },
+  {
+    id: "UwGSgJytufY",
+    tags: ["Sadhguru"],
+    title: "Sadhguru On How to Manifest What You Really Want",
+    views: "12,169,271",
+    viewsSmall: "12M",
+    releaseDate: "Jul 15, 2020",
+    releaseAgo: "year ago",
+    author: "Sadhguru",
+    subscribers: "8.13M",
+    likes: "8.9M",
+    unlikes: "265K",
+    description: `Sadhguru tells us how to take charge of our destiny by aligning our thought, emotions and energies to manifest what we really want.`,
+    image: stream5Img,
+    video: stream5Vid,
+  },
+  {
+    id: "LlY90lG_Fuw",
+    tags: ["PharellWilliams", "Freedom", "Pharell"],
+    title: "Pharrell Williams - Freedom (Video)",
+    views: "114,482,075",
+    viewsSmall: "114M",
+    releaseDate: "Jul 22, 2015",
+    releaseAgo: "6 years ago",
+    author: "Pharell Williams",
+    subscribers: "3.31M",
+    likes: "1M",
+    unlikes: "36K",
+    description: `Pharrell Williams' "Freedom" from the Despicable Me 3 Original Motion Picture Soundtrack
+    Listen to Pharrell: https://PharrellWilliams.lnk.to/listenYD`,
+    image: stream6Img,
+    video: stream6Vid,
+  },
+  {
+    id: "1RVRCd6J2NA",
+    tags: [],
+    title: "Pharrell Williams - Freedom (Video)",
+    views: "12,568,411",
+    viewsSmall: "12M",
+    releaseDate: "Oct 23, 2010",
+    releaseAgo: "11 years ago",
+    author: "WillSmithVEVO",
+    subscribers: "854K",
+    likes: "82K",
+    unlikes: "2.1K",
+    description: `Will Smith's official music video for 'Black Suits Comin' (Nod Ya Head)'. Click to listen to Will Smith on Spotify: http://smarturl.it/WillSmithSpotify?I...`,
+    image: stream7Img,
+    video: stream7Vid,
+  },
+  {
+    id: "gOsM-DYAEhY",
+    tags: [],
+    title: "Imagine Dragons - Whatever It Takes",
+    views: "770,228,717",
+    viewsSmall: "770M",
+    releaseDate: "Oct 12, 2017",
+    releaseAgo: "4 years ago",
+    author: "ImagineDragons",
+    subscribers: "25.8M",
+    likes: "4.7M",
+    unlikes: "131K",
+    description: `Get Imagine Dragons’ new album Evolve, out now: http://smarturl.it/EvolveID
+    Shop Imagine Dragons: http://smarturl.it/ImagineDragonsShop
+    Catch Imagine Dragons on tour: http://imaginedragonsmusic.com/tour`,
+    image: stream8Img,
+    video: stream8Vid,
+  },
+  {
+    id: "QtXby3twMmI",
+    tags: ["Coldplay", "AdventureOfALifetime", "AHeadFullOfDreams"],
+    title: "Coldplay - Adventure Of A Lifetime (Official Video)",
+    views: "1,217,730,523",
+    viewsSmall: "1.2B",
+    releaseDate: "Nov 29, 2015",
+    releaseAgo: "6 years ago",
+    author: "Coldplay",
+    subscribers: "19.3M",
+    likes: "4.7M",
+    unlikes: "265K",
+    description: `Coldplay - Adventure Of A Lifetime is taken from the album A Head Full of Dreams released in 2015`,
+    image: stream9Img,
+    video: stream9Vid,
+  },
+  {
+    id: "0KSOMA3QBU0",
+    tags: [],
+    title: "Katy Perry - Dark Horse (Official) ft. Juicy J",
+    views: "3,128,033,883",
+    viewsSmall: "3.1B",
+    releaseDate: "Feb 20, 2014",
+    releaseAgo: "7 years ago",
+    author: "Katy Perry",
+    subscribers: "41.3M",
+    likes: "13M",
+    unlikes: "1.3M",
+    description: `Listen to Katy’s new song “Smile”: https://katy.to/smileID
+
+    Get "Dark Horse" from Katy Perry's 'PRISM': http://katy.to/PRISM`,
+    image: stream10Img,
+    video: stream10Vid,
+  },
+
+  {
+    id: "JGwWNGJdvx8",
+    tags: ["EdSheeran", "ShapeOfYou", "divide"],
+    title: "Ed Sheeran - Shape of You (Official Music Video)",
+    views: "5,434,012,862",
+    viewsSmall: "5.4B",
+    releaseDate: "Jan 30, 2017",
+    releaseAgo: "4 years ago",
+    author: "Ed Sheeran",
+    subscribers: "48.5M",
+    likes: "27M",
+    unlikes: "1.3M",
+    description: `The official music video for Ed Sheeran - Shape Of You
+    Pre-order the new album "=" - out October 29th: https://es.lnk.to/equals`,
+    image: stream11Img,
+    video: stream11Vid,
   },
 ];
 
