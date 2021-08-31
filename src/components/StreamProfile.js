c3c80bd16a48cecb170b0cecaf27e19378526259
Rafał Łukawski <rs.lukawski@gmail.com>
2021-08-31 09:19:32 +0200

    subscribe status

--- src/components/StreamProfile.js
+++ src/components/StreamProfile.js
@@ -1,10 +1,18 @@
 import Icon from "../components/Icon";
 import Linkify from "react-linkify";
+import { useState } from "react";
 
 const StreamProfile = (props) => {
+  const [update, setUpdate] = useState(0);
   const stream = props.stream;
   const profileImg = stream.profileImage;
   let description = stream.description;
+
+  const setSubscribeStatus = (state) => {
+    stream.subscribeStatus = state;
+    setUpdate(update + 1);
+  };
+  let subscribeStatus = stream.subscribeStatus;
   description = description.split("\n").map((item, idx) => {
     return (
       <span key={idx}>
@@ -14,7 +22,6 @@ const StreamProfile = (props) => {
     );
   });
 
-  //   description = description.replaceAll("a", "<br />");
   return (
     <div className="main-profile">
       <img src={profileImg} className="profile-img" alt="Profile " />
@@ -28,7 +35,24 @@ const StreamProfile = (props) => {
         <p className="info-more">Show more</p>
       </div>
       <div className="profile-actions">
-        <button className="actions-subscribe">Subscribe</button>
+        {!subscribeStatus ? (
+          <button
+            className="action-subscribe"
+            onClick={() => setSubscribeStatus(true)}
+          >
+            Subscribe
+          </button>
+        ) : (
+          <>
+            <button
+              className="action-unsubscribe"
+              onClick={() => setSubscribeStatus(false)}
+            >
+              Subscribed
+            </button>
+            <Icon icon="notification" />
+          </>
+        )}
       </div>
     </div>
   );
