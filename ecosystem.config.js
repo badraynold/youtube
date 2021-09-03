7baf08548872e1b6d37754ce4fc2ba03b5e5cbe1
Rafał Łukawski <rs.lukawski@gmail.com>
2021-09-03 08:57:59 +0200

    ecosystem fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   script: "serve",
   env: {
-    PM2_SERVE_PATH: './build', 
-    PM2_SERVE_HOST: '127.0.0.1',
-    PM2_SERVE_PORT: 4000
-  }
-}
+    PM2_SERVE_PATH: "./build",
+    PM2_SERVE_HOST: "127.0.0.1",
+    PM2_SERVE_PORT: 4000,
+    PM2_SERVE_SPA: true,
+  },
+};
